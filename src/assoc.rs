f9baec5f1384926c192bd03c9409679fc5d17198
Sadie (zulc22) <zulc22db+oss@gmail.com>
2023-10-25 05:17:32 -0700

    finish assoc for linux

--- src/assoc.rs
+++ src/assoc.rs
@@ -24,9 +24,29 @@ pub fn register() -> std::io::Result<()> {
 
 #[cfg(target_os = "linux")]
 pub fn register() -> std::io::Result<()> {
-    use crate::term;
-    term::error_fatal("URI Registration - A Linux implementation of this using XDG is yet to be written.");
-    panic!();
+    use std::fs;
+    use std::path;
+    use std::io::{Error, ErrorKind};
+    use subprocess::Exec;
+
+    // Generate desktop entry
+    let desktop_entry = format!(r#"[Desktop Entry]
+Type=Application
+Name=mame-dl URI protocol handler
+Exec={} play %u
+StartupNotify=false
+MimeType=x-scheme-handler/mamedl;
+"#, std::env::current_exe()?.to_str().unwrap().replace(" ", "\\ "));
+    // Ensure ~/.local/share/applications exists
+    let applications = dirs::home_dir().unwrap().join(".local/share/applications");
+    fs::create_dir_all(applications.clone())?;
+    // Write desktop entry
+    fs::write(applications.join("scheme-mamedl.desktop"), desktop_entry)?;
+    // Register with MIME
+    if !Exec::shell("xdg-mime default scheme-mamedl.desktop x-scheme-handler/mamedl").capture().unwrap().exit_status.success() {
+        return Err(Error::from(ErrorKind::InvalidData));
+    }
+    return Ok(());
 }
 
 #[cfg(target_os = "macos")]
